8d44e39f00c72bae77d197fb36aeb2b6d0b353ae
Chris Hewitt <agop5134@gmail.com>
2022-01-14 16:43:01 -0700

    Add validations for 0/greater than 100 percent_off

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe BulkDiscount, type: :model do
   describe "relationships" do
     it {should belong_to(:merchant)}
   end
+
+  describe "validations" do
+      it { validate_presence_of(:percent_off) }
+      it { validate_numericality_of(:percent_off) }
+      it { should_not allow_value(0).for(:percent_off) }
+      it { should_not allow_value(100..).for(:percent_off) }
+  end
 end
